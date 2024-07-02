7182f1ab8d8ab17bcc5071f3b8072147b2f68c41
Steve Schwab <schwabs@umich.edu>
2024-07-02 12:59:07 -0700

    implement AuthForm

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,17 +6,6 @@ const jwt = require('jsonwebtoken')
 const Users = require('../users/users-model')
 
 router.post("/register", validateRoleName, (req, res, next) => {
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
   let { username, password, role_name } = req.body
   
   const hash = bcrypt.hashSync(password, BCRYPT_ROUNDS)
@@ -32,25 +21,6 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
   let { username, password } = req.body
 
   Users.findBy({ username })
--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,1 +1,48 @@
 // The form for registration and login.
+import React, { useState } from 'react'
+
+function AuthForm({ onResponse, onError }) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (action) => (e) => {
+        e.preventDefault()
+        const credentials = { username, password }
+
+        fetch(`http://localhost:9000/api/auth/${action}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials)
+        })
+            .then(res => res.json())
+            .then(onResponse)
+            .catch(onError)
+    }
+
+    return (
+        <form>
+            <div>
+                <input
+                    type='type'
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button onClick={handleSubmit('register')}>Register</button>
+                <button onClick={handleSubmit('login')}>Log in</button>
+            </div>
+        </form>
+    )
+}
+
+export default AuthForm
