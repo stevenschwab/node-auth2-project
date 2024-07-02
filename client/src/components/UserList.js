4dac8a9fe37cc432721f60f36a79f6e23c7dc4ac
Steve Schwab <schwabs@umich.edu>
2024-07-02 13:02:13 -0700

    implement UserList

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,1 +1,14 @@
 // The component to display the list of users
+import React, { useState } from 'react'
+
+function UserList({ users }) {
+    return (
+        <div id='userList'>
+            {users.map((user, index) => (
+                <div key={index}>{user.username}</div>
+            ))}
+        </div>
+    )
+}
+
+export default UserList
